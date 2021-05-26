33997cf032b81b38fea666fbe78860c7af86a98d
xXValhallaCoderXx <renate.gouveia@gmail.com>
2021-05-27 05:05:46 +0800

    Update lifecycle

--- android/app/src/main/java/com/custommoduleexample/OurModule.java
+++ android/app/src/main/java/com/custommoduleexample/OurModule.java
@@ -5,16 +5,34 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactMethod;
 import android.widget.Toast;
 
 
-public class OurModule extends ReactContextBaseJavaModule {
-    private ReactContext mReactContext;
+public class OurModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+    private ReactContext ctx;
 
     OurModule(ReactApplicationContext context) {
         super(context);
-        this.mReactContext = context;
+        this.ctx = context;
+        // Add this lide below
+        context.addLifecycleEventListener(this);
+    }
+
+    @Override
+    public void onHostResume() {
+        Toast.makeText(ctx, "Hello - I have mounted", 2500).show();
+    }
+
+    @Override
+    public void onHostPause() {
+
+    }
+
+    @Override
+    public void onHostDestroy() {
+
     }
 
     @Override
@@ -24,13 +42,12 @@ public class OurModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void openToast(String message) {
-        Toast.makeText(mReactContext, message, 2500).show();
+        Toast.makeText(ctx, message, 2500).show();
     }
 
     @ReactMethod
     public void heavyCalculation(Integer a, Integer b, Promise promise) {
         try {
-       
             promise.resolve((a + b * 4) + "");
         } catch(Exception e) {
             promise.reject("Computer says no", "Error Mpas", e);
